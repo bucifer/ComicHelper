c770ef9649f4e7c7963294a81c0621c6f0b68fcd
Terry Bu <ktbucifer@gmail.com>
2014-11-04 13:59:11 -0500

    Name and score validation in Create view

--- Jest Notes/Jest Notes/CreateViewController.m
+++ Jest Notes/Jest Notes/CreateViewController.m
@@ -40,10 +40,22 @@
 - (IBAction)createNewJokeAction:(id)sender {
     
     NSString *jokeTitle = self.titleField.text;
+    if (jokeTitle.length <= 0) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No joke title"
+                                                        message:@"You need to specify a joke title"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
     NSString *jokeScore = self.scoreField.text;
+    if ([self alertIfInvalid:jokeScore])
+        return; //cuts off and interrupts create new action if the alert ever goes up
+    
     NSString *jokeMinuteLength = self.lengthMinField.text;
     NSString *jokeSecondsLength = self.lengthSecondsField.text;
-    
     NSEntityDescription *entity = [NSEntityDescription entityForName:@"JokeCD" inManagedObjectContext:self.jokeDataManager.managedObjectContext];
     JokeCD *joke = [[JokeCD alloc] initWithEntity:entity insertIntoManagedObjectContext:self.jokeDataManager.managedObjectContext];
     joke.title = jokeTitle;
@@ -57,10 +69,31 @@
     
     [self.jokeDataManager.jokes addObject:[self.jokeDataManager convertCoreDataJokeIntoPresentationLayerJoke:joke]];
     
-    
     NSLog(@"New joke saved");
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+- (BOOL) alertIfInvalid: (NSString *) scoreString {
+    int tempScoreStore = [scoreString intValue];
+    if ([self isScoreInputValid:tempScoreStore] == FALSE) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Score out of range"
+                                                        message:@"Your score input is out of range. Please input a number between 0 to 10 (inclusive) under Joke Score"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return YES;
+    }
+    return FALSE;
+}
+
+- (BOOL) isScoreInputValid: (int) score {
+    if (score > 10 || score < 0) {
+        return FALSE;
+    }
+    return TRUE;
+}
+
 
 #pragma mark Keyboard Delegate methods
 
