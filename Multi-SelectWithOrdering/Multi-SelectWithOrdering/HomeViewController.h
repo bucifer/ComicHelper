5453bf84dca6ef3adc4b75eb030cb708aac10621
Terry Bu <ktbucifer@gmail.com>
2014-11-13 14:49:01 -0500

    About to switch from UITableViewController to a custom uiviewcontroller because you can't change tableheight in uitableviewcontroller sutpid

---
+++ Multi-SelectWithOrdering/Multi-SelectWithOrdering/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Multi-SelectWithOrdering
+//
+//  Created by Aditya Narayan on 11/13/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
