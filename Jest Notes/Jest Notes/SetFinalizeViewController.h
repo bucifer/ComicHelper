97487382ce1f8222ab86b44c0c32aa6ed54c813e
Terry Bu <ktbucifer@gmail.com>
2014-11-22 15:47:53 -0500

    Want to change joke title to joke name

--- Jest Notes/Jest Notes/SetFinalizeViewController.h
+++ Jest Notes/Jest Notes/SetFinalizeViewController.h
@@ -8,9 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SetFinalizeViewController : UIViewController
+@interface SetFinalizeViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) NSMutableArray *jokes;
 
+@property (strong, nonatomic) IBOutlet UILabel *setIDFillLabel;
+@property (strong, nonatomic) IBOutlet UITextField *setNameField;
+@property (strong, nonatomic) IBOutlet UILabel *setLengthFillLabel;
+
+
+- (IBAction)createSetButton:(id)sender;
 
 @end
--- Jest Notes/Jest Notes/SetFinalizeViewController.m
+++ Jest Notes/Jest Notes/SetFinalizeViewController.m
@@ -34,4 +34,6 @@
 }
 */
 
+- (IBAction)createSetButton:(id)sender {
+}
 @end
