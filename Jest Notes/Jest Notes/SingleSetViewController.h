a3937485fb947f354d43dafdf00f84d1062f5cca
Terry Bu <ktbucifer@gmail.com>
2014-11-22 19:01:19 -0500

    Started implementing single set view - still need to implement a tableview to show all the NSSet jokes ... need to figure that out plus ordering

---
+++ Jest Notes/Jest Notes/SingleSetViewController.h
@@ -0,0 +1,17 @@
+//
+//  SingleSetViewController.h
+//  Jest Notes
+//
+//  Created by Aditya Narayan on 11/22/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SetCD.h"
+
+@interface SingleSetViewController : UIViewController
+
+@property (strong, nonatomic) SetCD;
+
+
+@end
